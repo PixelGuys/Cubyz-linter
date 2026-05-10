dda7fcbb70d8ad237528e7560c1bd998fd560f9d
IntegratedQuantum <43880493+IntegratedQuantum@users.noreply.github.com>
2026-05-10 17:09:54 +0200

    Add import checks (#2)
    Divides the file into two part, the import/alias section where imports
    are allowed, and the rest of the file where imports aren't allowed. This
    ensures that all imports are allowed.
    
    Valid imports are
    - `<pub?> const <name> = <rhs>;`
    - `<pub?> const @"<any>:<name>" = <rhs>;`
    - `<pub?> const @"<path>:<name>" = <rhs>;`
    - `<pub?> const <any> = @import("<any2>/_list.zig");`
    
    where `<rhs>` can be one of
    - `<path>.<name>`
    - `@import("<name>");`
    - `@import("<name>.zig");`
    - `@import("<name>.zon");`
    - `@import("<path>/<name>");`
    - `@import("<path>/<name>.zig");`
    - `@import("<path>/<name>.zon");`
    When `<path>` or `<name>` must be identical on both sides. <path> can
    include multiple paths
    `<pub?>` can be either `pub` or nothing
    
    Known problems so far:
    - `const Atomic = std.atomic.Value;` → we could make an exception for
    these
    - `_command.zig` is a problem → should probably not be in the same
    folder as the commands, but I don't want to move anything until the arg
    parser is done
    
    The order of imports is currently not checked.
    
    - [x] fix violations downstream before attempting to merge this
    
    progress towards https://github.com/PixelGuys/Cubyz/issues/1449
    
    ---------
    
    Co-authored-by: Wunka <56411012+Wunka@users.noreply.github.com>

--- src/main.zig
+++ src/main.zig
@@ -6,7 +6,7 @@ var allocator: std.mem.Allocator = undefined;
 
 var failed: bool = false;
 
-fn printError(msg: []const u8, filePath: []const u8, data: []const u8, charIndex: usize) void {
+fn getLineData(data: []const u8, charIndex: usize) struct { start: usize, end: usize, number: usize } {
 	var lineStart: usize = 0;
 	var lineNumber: usize = 1;
 	var lineEnd: usize = data.len;
@@ -22,10 +22,15 @@ fn printError(msg: []const u8, filePath: []const u8, data: []const u8, charIndex
 			break;
 		}
 	}
+	return .{.start = lineStart, .end = lineEnd, .number = lineNumber};
+}
+
+fn printError(msg: []const u8, filePath: []const u8, data: []const u8, charIndex: usize) void {
+	const line = getLineData(data, charIndex);
 
 	var startLineChars: std.ArrayList(u8) = .empty;
 	defer startLineChars.deinit(allocator);
-	for (data[lineStart..charIndex]) |c| {
+	for (data[line.start..charIndex]) |c| {
 		if (c == '\t') {
 			startLineChars.append(allocator, '\t') catch {};
 		} else {
@@ -35,12 +40,116 @@ fn printError(msg: []const u8, filePath: []const u8, data: []const u8, charIndex
 
 	failed = true;
 
-	std.log.err("Found formatting error in line {} of file {s}: {s}\n{s}\n{s}^", .{lineNumber, filePath, msg, data[lineStart..lineEnd], startLineChars.items});
+	std.log.err("{s}:{}:{}: {s}\n{s}\n{s}^", .{filePath, line.number, charIndex - line.start + 1, msg, data[line.start..line.end], startLineChars.items});
+}
+
+fn printInfo(msg: []const u8, filePath: []const u8, data: []const u8, charIndex: usize) void {
+	const line = getLineData(data, charIndex);
+
+	var startLineChars: std.ArrayList(u8) = .empty;
+	defer startLineChars.deinit(allocator);
+	for (data[line.start..charIndex]) |c| {
+		if (c == '\t') {
+			startLineChars.append(allocator, '\t') catch {};
+		} else {
+			startLineChars.append(allocator, ' ') catch {};
+		}
+	}
+
+	failed = true;
+
+	std.log.info("{s}:{}:{}: {s}\n{s}\n{s}^", .{filePath, line.number, charIndex - line.start + 1, msg, data[line.start..line.end], startLineChars.items});
+}
+
+fn isAliasAllowed(_importName: []const u8, _aliasName: []const u8) bool {
+	var importName = _importName;
+	var aliasName = _aliasName;
+
+	if (importName.len != 0 and importName[0] == '"') importName = std.mem.trim(u8, importName, "\"");
+
+	if (std.mem.endsWith(u8, importName, "_list.zig")) return true;
+	if (std.mem.eql(u8, aliasName, "Atomic") and std.mem.eql(u8, importName, "Value")) return true;
+
+	if (std.mem.endsWith(u8, importName, ".zon")) importName.len -= 4;
+	if (std.mem.endsWith(u8, importName, ".zig")) importName.len -= 4;
+
+	if (importName.len != 0 and importName[0] == '@') importName = importName[2 .. importName.len - 1];
+	if (aliasName.len != 0 and aliasName[0] == '@') aliasName = aliasName[2 .. aliasName.len - 1];
+
+	if (std.mem.eql(u8, importName, aliasName)) return true;
+
+	if (std.mem.findLast(u8, importName, "/")) |i| importName = importName[i + 1 ..];
+
+	if (std.mem.findLast(u8, importName, ":")) |i| importName = importName[i + 1 ..];
+	if (std.mem.findLast(u8, aliasName, ":")) |i| aliasName = aliasName[i + 1 ..];
+
+	if (std.mem.eql(u8, importName, "world") and std.mem.eql(u8, aliasName, "world_zig")) return true; // TODO: Remove after https://github.com/PixelGuys/Cubyz/issues/3069
+
+	return std.mem.eql(u8, importName, aliasName);
+}
+fn checkImports(ast: *std.zig.Ast, filePath: []const u8) void {
+	const root = ast.rootDecls();
+	var firstNonImportNode: ?std.zig.Ast.Node.Index = null;
+
+	for (root) |node| {
+		const isImport: bool = blk: {
+			if (ast.nodeTag(node) != .simple_var_decl) break :blk false;
+			const varDec = ast.simpleVarDecl(node);
+			const aliasName = ast.tokenSlice(varDec.ast.mut_token + 1);
+			const rhsNode = varDec.ast.init_node.unwrap().?;
+
+			switch (ast.nodeTag(rhsNode)) {
+				.builtin_call_two_comma => { // @import("x",)
+					const token = ast.nodeMainToken(rhsNode);
+					const importKeyword = ast.tokenSlice(token);
+					if (!std.mem.eql(u8, importKeyword, "@import")) break :blk false;
+					printError("@import should not have a trailing comma.", filePath, ast.source, ast.tokenStart(token));
+					break :blk true;
+				},
+				.builtin_call_two => { // @import("x")
+					const importKeyword = ast.tokenSlice(ast.nodeMainToken(rhsNode));
+					if (!std.mem.eql(u8, importKeyword, "@import")) break :blk false;
+
+					const token = ast.nodeMainToken(ast.nodeData(rhsNode).opt_node_and_opt_node[0].unwrap().?);
+					var importName = ast.tokenSlice(token);
+					importName = importName[1 .. importName.len - 1];
+
+					if (!isAliasAllowed(importName, aliasName)) {
+						printError("Encountered import with mismatched name", filePath, ast.source, ast.tokenStart(token));
+					}
+					break :blk true;
+				},
+				.field_access => { // alias
+					const token = ast.nodeData(rhsNode).node_and_token[1];
+					const importName = ast.tokenSlice(token);
+
+					if (!isAliasAllowed(importName, aliasName)) {
+						if (firstNonImportNode != null) break :blk false;
+						printError("Encountered alias with mismatched name", filePath, ast.source, ast.tokenStart(token));
+					}
+					break :blk true;
+				},
+				else => break :blk false,
+			}
+		};
+		if (isImport) {
+			if (firstNonImportNode) |nonImportNode| {
+				printError("Encountered import/alias after import section", filePath, ast.source, ast.tokenStart(ast.firstToken(node)));
+				printInfo("determined end of import section", filePath, ast.source, ast.tokenStart(ast.firstToken(nonImportNode)));
+			}
+		} else {
+			if (firstNonImportNode == null) {
+				firstNonImportNode = node;
+			}
+		}
+	}
 }
 
 fn checkFile(dir: std.Io.Dir, filePath: []const u8) !void {
-	const data = try dir.readFileAlloc(io, filePath, allocator, .unlimited);
+	const data = try dir.readFileAllocOptions(io, filePath, allocator, .unlimited, .@"1", 0);
 	defer allocator.free(data);
+	var ast = try std.zig.Ast.parse(allocator, data, .zig);
+	defer ast.deinit(allocator);
 
 	var lineStart: bool = true;
 
@@ -79,6 +188,9 @@ fn checkFile(dir: std.Io.Dir, filePath: []const u8) !void {
 	}
 	if (data.len != 0 and data[data.len - 1] != '\n' or (data.len > 2 and data[data.len - 2] == '\n')) {
 		printError("File should end with a single empty line", filePath, data, data.len - 1);
+	}
+	if (std.mem.endsWith(u8, filePath, ".zig")) {
+		checkImports(&ast, filePath);
 	}
 }
 
