22440d89b28121ddecf2a7c643d89c69dad7b295
IntegratedQuantum <jahe788@gmail.com>
2026-05-30 17:15:09 +0200

    Fix indention checker

--- src/rules/tab_indentation.zig
+++ src/rules/tab_indentation.zig
@@ -3,7 +3,14 @@ const std = @import("std");
 const main = @import("main");
 
 pub fn check(ctx: main.Context) void {
-	if (std.mem.find(u8, ctx.data, "\n ")) |index| {
-		ctx.printError("Incorrect indentation. Please use tabs instead of spaces.", index + 1);
+	var lineStart: bool = true;
+	for (ctx.data, 0..) |char, i| {
+		if (lineStart and char == '\t') continue;
+		if (lineStart and char == ' ') {
+			ctx.printError("Incorrect indentation. Please use tabs instead of spaces.", i);
+			lineStart = false;
+			continue;
+		}
+		lineStart = char == '\n';
 	}
 }
