0c87ab44bf3d109f50352f71e7773951fa281bed
Wunka <56411012+Wunka@users.noreply.github.com>
2026-06-04 17:30:11 +0200

    maybe?

--- src/rules/comment_start.zig
+++ src/rules/comment_start.zig
@@ -3,7 +3,9 @@ const std = @import("std");
 const main = @import("main");
 
 pub fn check(ctx: main.Context) void {
-	for (0..ctx.data.len - 3) |i| {
+	if (ctx.data.len <= 3) return;
+
+	for (0..ctx.data.len - 3) |i| { //extra
 		if (ctx.data[i] != '/' or ctx.data[i + 1] != '/') continue;
 		const commentStart = if (ctx.data[i + 2] == '/' or ctx.data[i + 2] == '!') i + 3 else i + 2;
 		if (ctx.data[commentStart] != ' ' and ctx.data[commentStart] != '\n') {
