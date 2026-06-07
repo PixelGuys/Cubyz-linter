85fc4cd6d8e98cb3e1a00745cb82aa7f98fdf2e8
Wunka <56411012+Wunka@users.noreply.github.com>
2026-06-07 22:18:13 +0200

    fix rule

--- src/rules/trailing_space.zig
+++ src/rules/trailing_space.zig
@@ -3,6 +3,8 @@ const std = @import("std");
 const main = @import("main");
 
 pub fn check(ctx: main.Context) void {
+	if (ctx.data.len < 1) return;
+
 	for (0..ctx.data.len - 1) |i| {
 		if ((ctx.data[i] == ' ' or ctx.data[i] == '\t') and ctx.data[i + 1] == '\n') {
 			ctx.printError("Line contains trailing whitespaces. Please remove them.", i);
