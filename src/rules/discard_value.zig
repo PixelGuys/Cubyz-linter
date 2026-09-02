76c5935f91eae6164113f136fbf50ee2e0acac6b
Wunka <56411012+Wunka@users.noreply.github.com>
2026-09-02 15:49:37 +0200

    add & support

--- src/rules/discard_value.zig
+++ src/rules/discard_value.zig
@@ -12,11 +12,14 @@ pub fn check(ctx: main.Context) void {
 		const data = ast.nodeData(node).node_and_node;
 
 		const name = data.@"0".toOptional().unwrap() orelse continue;
-		const expression = data.@"1".toOptional().unwrap() orelse continue;
+		var expression = data.@"1".toOptional().unwrap() orelse continue;
+
+		while (ast.nodeTag(expression) == .address_of) {
+			expression = ast.nodeData(expression).node;
+		}
 
 		if (ast.nodeTag(name) != .identifier) continue;
 		if (ast.nodeTag(expression) != .identifier) continue;
-
 		if (!std.mem.eql(u8, ast.getNodeSource(name), "_")) continue;
 
 		ctx.printError("Do not discard variables, for parameters discard the parameter directly", ast.tokenStart(ast.nodeMainToken(node)));
