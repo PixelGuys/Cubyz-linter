b9bd7cd70fde4e8d12a65179c4f07283575b9e7f
Wunka <56411012+Wunka@users.noreply.github.com>
2026-09-02 15:41:41 +0200

    add rule for discarding of values

---
+++ src/rules/discard_value.zig
@@ -0,0 +1,24 @@
+const std = @import("std");
+
+const main = @import("main");
+
+pub fn check(ctx: main.Context) void {
+	const ast = ctx.ast orelse return;
+
+	for (0..ast.nodes.len) |nodeIndex| {
+		const node: std.zig.Ast.Node.Index = @enumFromInt(nodeIndex);
+		if (ast.nodeTag(node) != .assign) continue;
+
+		const data = ast.nodeData(node).node_and_node;
+
+		const name = data.@"0".toOptional().unwrap() orelse continue;
+		const expression = data.@"1".toOptional().unwrap() orelse continue;
+
+		if (ast.nodeTag(name) != .identifier) continue;
+		if (ast.nodeTag(expression) != .identifier) continue;
+
+		if (!std.mem.eql(u8, ast.getNodeSource(name), "_")) continue;
+
+		ctx.printError("Do not discard variables, for parameters discard the parameter directly", ast.tokenStart(ast.nodeMainToken(node)));
+	}
+}
